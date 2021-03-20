546cb2fe9dfc3376d665d3781511f54f2046d1ff
Tanaca <jtanacar@gmail.com>
2021-03-20 16:41:17 -0300

    Adicionando prettier e eslint

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,17 @@
+import React from 'react'
 import Document, { DocumentContext } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx:DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
         })
-
       const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
@@ -21,11 +20,10 @@ export default class MyDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
+        )
       }
     } finally {
       sheet.seal()
     }
   }
 }
-
